2d40749021940775b925f751a369509341a82b85
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-02-15 17:29:50 -0300

    Creo la funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Esta aplicacion permite sortear un nombre al azar de una lista previamente ingresada para el juego 
+// del amigo secreto
+
+//Se valida el ingreso para que el usuario este obligado a ingresar un nombre
+
+const listaAmigos = document.getElementById("listaAmigos");
+let listaAmigoSecreto = []; //Defino el valor inicial del array
+let nombreAmigo="";
+let nombre="";
+let coincidencia=false; //Defino esta variable para evaluar si hay coincidencias
+listaAmigos.innerHTML ="";
+
+
+function agregarAmigo() {
+
+    //Limpio el texto del resultado 
+    resultado.innerHTML =""
+
+    //Obtengo el valor ingresado por el usuario en el cuadro de texto...
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    //Si no ha ingresado un nombre y presiona Agregar envio una alerta...
+    if (nombreAmigo ==='') {
+        alert ('Por favor, inserte un nombre');
+        limpiarCuadrodeTexto();
+
+    } else {
+
+        limpiarCuadrodeTexto();
+        
+        validarDuplicidad(nombreAmigo);
+        
+        //Si la funcion anterior no encontro duplicados, continuo con el proceso...
+        if (coincidencia===false) {
+            listaAmigoSecreto.push(nombreAmigo);
+            listaAmigos.innerHTML += `<li> ${nombreAmigo}</li>`;
+        }
+                
+    }
+    return;
+}
